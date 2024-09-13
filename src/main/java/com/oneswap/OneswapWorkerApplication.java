f02b5296f64b8795344bab7287d02609d75e1fae
Drinkaiii <kai410705@gmail.com>
2024-09-13 11:56:08 +0800

    [init]

---
+++ src/main/java/com/oneswap/OneswapWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.oneswap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneswapWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneswapWorkerApplication.class, args);
+	}
+
+}
