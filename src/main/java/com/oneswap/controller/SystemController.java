4b301ca5143ede031629e5f6adcee872b1457372
Drinkaiii <kai410705@gmail.com>
2024-10-03 16:47:49 +0800

    [chore] add health check endpoint for load balancer

---
+++ src/main/java/com/oneswap/controller/SystemController.java
@@ -0,0 +1,19 @@
+package com.oneswap.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/1.0/system")
+public class SystemController {
+
+    @GetMapping("/health")
+    public ResponseEntity<?> healthCheck(){
+        return new ResponseEntity("data", HttpStatus.OK);
+    }
+    
+}
